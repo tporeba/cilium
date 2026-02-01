4dd6a6299ddb18f8c64daa6b3f926f28a43514b7
Tomasz Poreba <tomasz.poreba@siemens.com>
2026-02-01 18:34:38 +0100

    enable go agent

--- hubble/main.go
+++ hubble/main.go
@@ -8,9 +8,15 @@ import (
 	"os"
 
 	"github.com/cilium/cilium/hubble/cmd"
+	"github.com/google/gops/agent"
 )
 
 func main() {
+	// Start the gops agent with default options.
+	if err := agent.Listen(agent.Options{}); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
 	if err := cmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(1)
